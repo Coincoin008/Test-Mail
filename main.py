71359c671d4f28486ab0072559b439d44d48e264
Coincoin008 <88879392+Coincoin008@users.noreply.github.com>
2022-11-15 19:25:35 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -3,15 +3,17 @@ import pyautogui, smtplib, ssl
 
 port = 587  # For starttls
 smtp_server = "smtp.gmail.com"
-sender_email = "erabaroust@gmail.com"
+sender_email = "smam.elliot@gmail.com"
 receiver_email = "marc.lefevre@noos.fr"
-password = "cmymrwjcfmqsaoxr"
+password = "obcbfuncgjxzdvti"
 message = pyautogui.prompt(text="", title="Quel message veux  tu t'envoyer ?", default="")
 
-context = ssl.create_default_context()
-with smtplib.SMTP(smtp_server, port) as server:
-    server.ehlo()  # Can be omitted
-    server.starttls(context=context)
-    server.ehlo()  # Can be omitted
-    server.login(sender_email, password)
-    server.sendmail(sender_email, receiver_email, message)
+if __name__ == "__main__":
+    
+    context = ssl.create_default_context()
+    with smtplib.SMTP(smtp_server, port) as server:
+        server.ehlo()  # Can be omitted  
+        server.starttls(context=context)
+        server.ehlo()  # Can be omitted
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message)
